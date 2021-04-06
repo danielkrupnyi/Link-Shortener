dfe21703d43758d6ef8dd4351078391ae8b3389c
Daniel Krupnyy <danielkrupnyy@Daniels-MacBook-Air.local>
2021-04-06 13:36:29 +0300

    changed constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-const HTTP_URL_VALIDATOR_REGEX = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+const HTTP_URL_VALIDATOR_REGEX = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g;
 const baseURL = `https://api.shrtco.de/v2/`;
 
 export { HTTP_URL_VALIDATOR_REGEX, baseURL };
